8eb131308e5b8cb313405aa552fc679ff01f2d91
nfabry87-cell <nfabry87@gmail.com>
2026-03-18 08:15:44 +0100

    Update daily_predictions.py

--- daily_predictions.py
+++ daily_predictions.py
@@ -63,8 +63,8 @@ email = os.getenv("nfabry87@gmail.com")         # Email to send recommendations 
 # ---------------------------------------------------
 
 # Load environment variables and login to kickbase
-USERNAME = os.getenv("noah.fabry@icloud.com") # DO NOT CHANGE THIS, YOU MUST SET THOSE IN GITHUB SECRETS OR A .env FILE
-PASSWORD = os.getenv("No Fa 2008") # DO NOT CHANGE THIS, YOU MUST SET THOSE IN GITHUB SECRETS OR A .env FILE
+USERNAME = os.getenv("KICK_USER") # DO NOT CHANGE THIS, YOU MUST SET THOSE IN GITHUB SECRETS OR A .env FILE
+PASSWORD = os.getenv("KICK_PASS") # DO NOT CHANGE THIS, YOU MUST SET THOSE IN GITHUB SECRETS OR A .env FILE
 token = login(USERNAME, PASSWORD)
 print("\nLogged in to Kickbase.")
 
